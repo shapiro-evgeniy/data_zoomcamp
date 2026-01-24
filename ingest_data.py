88358176cfb2a162671fb9ccd830208ac9ecdaaf
Evgeny Shapiro <eshapiro@varonis.com>
2026-01-24 09:30:02 +0200

    Add parquet supporting

--- ingest_data.py
+++ ingest_data.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+import ssl
 import pandas as pd
 from sqlalchemy import create_engine
 from tqdm.auto import tqdm 
 import click
+import urllib.request
 
 dtype = {
     "VendorID": "Int64",
@@ -60,46 +62,52 @@ parse_dates = [
 @click.option('--pg-host', default='localhost', help='PostgreSQL host')
 @click.option('--pg-port', default='5432', help='PostgreSQL port')
 @click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
-@click.option('--year', default=2021, type=int, help='Year of the data')
-@click.option('--month', default=1, type=int, help='Month of the data')
+@click.option('--year', default=2025, type=int, help='Year of the data')
+@click.option('--month', default=11, type=int, help='Month of the data')
 @click.option('--chunksize', default=100000, type=int, help='Chunk size for ingestion')
 @click.option('--target-table', default='yellow_taxi_data', help='Target table name')
 def run(pg_user, pg_pass, pg_host, pg_port, pg_db, year, month, chunksize, target_table):
-    prefix='https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow'
-    url=f'{prefix}/yellow_tripdata_{year}-{month:02d}.csv.gz'
+    prefix='https://d37ci6vzurychx.cloudfront.net/trip-data'
+    url=f'{prefix}/green_tripdata_{year}-{month:02d}.parquet'
+    local_file = f'green_tripdata_{year}-{month:02d}.parquet'
 
+    print("After url creation")
     engine = create_engine(f'postgresql://{pg_user}:{pg_pass}@{pg_host}:{pg_port}/{pg_db}')
+    print("After engine creation")
 
-    df_iter = pd.read_csv(
-        url,
-        dtype=dtype,
-        parse_dates=parse_dates,
-        iterator=True,
-        chunksize=chunksize
+    # Create unverified SSL context
+    ssl_context = ssl._create_unverified_context()
+    urllib.request.urlretrieve(url, local_file, context=ssl_context)
+    print(f"Downloaded to {local_file}")
+
+    print(f"Before read parquet {local_file}")
+    df = pd.read_parquet(
+        local_file,
+        engine='pyarrow'
     )
+    print("After read parquet")
 
-    first = True
+    print("Before create table")
+    # Create table schema (no data)
+    df.head(0).to_sql(
+        name=target_table,
+        con=engine,
+        if_exists="replace"
+    )
+    print("Table created")
 
-    for df_chunk in tqdm(df_iter):
+    # Insert data in chunks
+    total_rows = len(df)
+    for i in tqdm(range(0, total_rows, chunksize)):
+        df_chunk = df.iloc[i:i+chunksize]
+        
+        # df_chunk.to_sql(
+        #     name=target_table,
+        #     con=engine,
+        #     if_exists="append"
+        #)
 
-        if first:
-            # Create table schema (no data)
-            df_chunk.head(0).to_sql(
-                name=target_table,
-                con=engine,
-                if_exists="replace"
-            )
-            first = False
-            print("Table created")
-
-        # Insert chunk
-        df_chunk.to_sql(
-            name=target_table,
-            con=engine,
-            if_exists="append"
-        )
-
-        print("Inserted:", len(df_chunk))
+        print(f"Inserted: {len(df_chunk)} rows")
 
 if __name__ == "__main__":
     run()
